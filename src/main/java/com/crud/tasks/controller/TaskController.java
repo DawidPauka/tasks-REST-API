999647848f30042a6184a9fd844600f98b83ad32
“nasz_login_do_github <nasz_email_do_github>
2018-09-15 22:35:27 +0200

    15.2. Tworzenie połączenia z bazą danych z poziomu aplikacji

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -4,15 +4,21 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
+
+
 public class TaskController {
+
     @Autowired
     private DbService service;
+
     @Autowired
     private TaskMapper taskMapper;
 
