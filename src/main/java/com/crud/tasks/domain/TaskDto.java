b926dd30d5b99d6bc23c121758edd984e4bd27ed
“nasz_login_do_github <nasz_email_do_github>
2018-09-15 18:29:17 +0200

    15.2. Tworzenie połączenia z bazą danych z poziomu aplikacji

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -1,16 +1,14 @@
 package com.crud.tasks.domain;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
-
-
+@AllArgsConstructor
 public class TaskDto {
     private Long id;
-    private  String title;
-    private  String content;
-
+    private String title;
+    private String content;
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+
+
 public class DbService {
     @Autowired
     private TaskRepository repository;
